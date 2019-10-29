c54778f299759a7425b106978b30ea9c4f72b1dd
rico.putra <rico.putra@outlook.co.id>
2019-10-29 16:10:26 +0700

    [UPDATE] Prepare dataBinding and implement navigate to next page in LoginActivity

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/viewmodels/LoginActivityViewModel.kt
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LoginActivityViewModel : ViewModel() {
+
+    val username: String? = null
+    val password: String? = null
+
+
+}
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/LoginActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/LoginActivity.kt
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginBtnClicked(v: View) {
+        val intent: Intent = Intent(this, MenuCategoryActivity::class.java)
+        startActivity(intent)
     }
 }
