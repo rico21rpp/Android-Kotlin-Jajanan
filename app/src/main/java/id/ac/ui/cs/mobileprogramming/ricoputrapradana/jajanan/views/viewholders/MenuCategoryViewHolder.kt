46025de815c167b9f787ac32b8105997315411f5
rico.putra <rico.putra@outlook.co.id>
2019-10-29 21:01:35 +0700

    [UPDATE] Implement view holder

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/viewholders/MenuCategoryViewHolder.kt
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views.viewholders
+
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.models.MenuCategory
+import kotlinx.android.synthetic.main.fragment_menu_category.view.*
+import java.util.zip.Inflater
+
+class MenuCategoryViewHolder(inflater: LayoutInflater, parent: ViewGroup)
+    : RecyclerView.ViewHolder(inflater.inflate(R.layout.menu_category_list_item)) {
+
+    private var menuNameTextView: TextView? = null
+
+    init {
+        menuNameTextView = itemView.findViewById(R.id.item_name)
+    }
+
+    fun bind(menu: MenuCategory) {
+        menuNameTextView?.text = menu.category
+    }
+}
