51da2bb79a05a0b458c6c8eac4efc3e5e5331845
rico.putra <rico.putra@outlook.co.id>
2019-10-29 20:22:29 +0700

    [ADD] Initiate MenuCategoryFragment for populating recyclerView of menu categories

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/MenuCategoryActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/MenuCategoryActivity.kt
@@ -3,11 +3,20 @@ package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views.fragments.MenuCategoryFragment
 
 class MenuCategoryActivity : AppCompatActivity() {
+
+    private val fragmentManager = supportFragmentManager
+    private val fragmentTransaction = fragmentManager.beginTransaction()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_category)
+
+        val menuCategoryFragment = MenuCategoryFragment()
+        fragmentTransaction.add(R.id.menu_category_fragment, menuCategoryFragment)
+        fragmentTransaction.commit()
+
     }
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/fragments/MenuCategoryFragment.kt
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
+
+class MenuCategoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_category, container, false)
+    }
+
+
+}
