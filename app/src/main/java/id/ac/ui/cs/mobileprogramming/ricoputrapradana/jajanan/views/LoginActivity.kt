ea95a1d42297a828ea9a574c51fb85a1cb95bff8
rico.putra <rico.putra@outlook.co.id>
2019-10-29 15:40:50 +0700

    [ADD] Prepare UI for LoginActivity

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/LoginActivity.kt
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
