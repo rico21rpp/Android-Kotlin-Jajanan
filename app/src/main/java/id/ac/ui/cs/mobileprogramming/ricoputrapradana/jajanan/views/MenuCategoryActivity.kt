039753624da423a386bff2b0b48f7f334cd181ea
rico.putra <rico.putra@outlook.co.id>
2019-10-29 16:02:50 +0700

    [ADD] Prepare MenuCategoryActivity

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/views/MenuCategoryActivity.kt
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ui.cs.mobileprogramming.ricoputrapradana.jajanan.R
+
+class MenuCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_category)
+    }
+}
