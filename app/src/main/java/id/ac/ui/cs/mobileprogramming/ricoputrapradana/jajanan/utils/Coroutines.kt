3eb167d65631b9ecae50169df18501de841cfa78
rico.putra <rico.putra@outlook.co.id>
2019-10-31 21:09:13 +0700

    [UPDATE] Add some documentations for Coroutines object

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/utils/Coroutines.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ricoputrapradana/jajanan/utils/Coroutines.kt
@@ -4,8 +4,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+/**
+ * static class for calling the "main" method
+ */
 object Coroutines {
 
+    /**
+     * Running "work" codes in asynchronous mechanism in Coroutine Scope
+     */
     fun main(work: suspend (() -> Unit)) =
         CoroutineScope(Dispatchers.Main).launch {
             work()
